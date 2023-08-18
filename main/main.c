6be3d593d3f577496e14cf50587a00ba4c47a6a5
JoshuaJWatt <Contact@joshuawatt.com>
2023-08-18 21:23:31 +0100

    Actual initial commit

---
+++ main/main.c
@@ -0,0 +1,15 @@
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "esp_error.h"
+
+esp_err_t setup(void)
+{
+	esp_err_t ret = ESP_OK;
+
+	return(ret);
+}
+
+void main(void)
+{
+
+};
